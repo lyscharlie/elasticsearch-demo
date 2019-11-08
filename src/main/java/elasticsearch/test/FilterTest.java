0c7d115acb60763d096964c81b0332671f8d7421
lyscharlie <lyscharlie@hotmail.com>
2019-11-08 18:13:36 +0800

    11111

---
+++ src/main/java/elasticsearch/test/FilterTest.java
@@ -0,0 +1,98 @@
+package elasticsearch.query.fulltext;
+
+import java.util.List;
+
+import org.elasticsearch.action.bulk.BulkItemResponse;
+import org.elasticsearch.action.bulk.BulkResponse;
+import org.elasticsearch.action.search.SearchResponse;
+import org.elasticsearch.client.RestHighLevelClient;
+import org.elasticsearch.index.query.MatchQueryBuilder;
+import org.elasticsearch.index.query.Operator;
+import org.elasticsearch.index.query.QueryBuilders;
+import org.elasticsearch.search.SearchHit;
+import org.elasticsearch.search.builder.SearchSourceBuilder;
+import org.elasticsearch.search.fetch.subphase.highlight.HighlightBuilder;
+
+import elasticsearch.common.BaseDocument;
+import elasticsearch.common.ElasticsearchUtils;
+import elasticsearch.query.QueryTestUtils;
+
+/**
+ * 分词匹配
+ *
+ * @author liyishi
+ */
+public class MatchQueryTest {
+
+	public static void main(String[] args) {
+
+		try {
+			String keyword = "羽绒服";
+
+			List<BaseDocument> dataList = QueryTestUtils.chineseList();
+
+			String index = "demo_test";
+			String mappings = QueryTestUtils.mappings();
+
+			// 连接elasticsearch
+			RestHighLevelClient client = QueryTestUtils.initClient();
+
+			// 创建index
+			if (ElasticsearchUtils.checkIndexExist(client, index)) {
+				ElasticsearchUtils.removeIndex(client, index);
+			}
+			ElasticsearchUtils.createIndex(client, index, mappings);
+
+			QueryTestUtils.line("完成创建索引");
+
+			// 批量添加数据
+			BulkResponse bulkResponse = ElasticsearchUtils.saveBulkDocs(client, index, dataList, true);
+			for (BulkItemResponse bulkItemResponse : bulkResponse.getItems()) {
+				System.out.println(bulkItemResponse.getResponse().getId());
+			}
+
+			QueryTestUtils.line("完成写入");
+
+			// 关键字
+			MatchQueryBuilder matchQueryBuilder = QueryBuilders.matchQuery("desc", keyword);
+			matchQueryBuilder.operator(Operator.AND);
+
+			// 高亮
+			HighlightBuilder highlightBuilder = new HighlightBuilder();
+			highlightBuilder.preTags("<strong>");// 设置前缀
+			highlightBuilder.postTags("</strong>");// 设置后缀
+			highlightBuilder.field("desc");// 设置高亮字段
+
+			// 查询数据
+			SearchSourceBuilder searchSourceBuilder = new SearchSourceBuilder();
+			searchSourceBuilder.from(0);
+			searchSourceBuilder.size(10);
+			searchSourceBuilder.query(matchQueryBuilder);
+			searchSourceBuilder.highlighter(highlightBuilder);
+			System.out.println(searchSourceBuilder);
+
+			QueryTestUtils.line();
+
+			SearchResponse response = ElasticsearchUtils.getDocsByQuery(client, index, searchSourceBuilder);
+			System.out.println(response.toString());
+
+			QueryTestUtils.line();
+
+			if (response.getHits().getTotalHits().value > 0) {
+				for (SearchHit item : response.getHits().getHits()) {
+					System.out.println(item.getScore() + "==>" + item.getHighlightFields() + " ==> "+ item.getSourceAsString());
+				}
+			}
+
+			// 删除index
+			ElasticsearchUtils.removeIndex(client, index);
+
+			// 关闭
+			client.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+	}
+
+}
